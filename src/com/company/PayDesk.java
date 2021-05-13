a2882362af79283cd5c410276b3ab033061e63c4
orysiayashchun <54995877+orysiayashchun@users.noreply.github.com>
2021-05-13 08:58:55 +0300

    Added files

--- src/com/company/PayDesk.java
+++ src/com/company/PayDesk.java
@@ -5,19 +5,51 @@ import Posterity.Puzzle;
 import Posterity.SoftToy;
 
 import javax.script.ScriptException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class PayDesk {
 
-    public static void main(String[] args)   {
+    public static void main(String[] args) throws IOException   {
         Date date=new Date();
         ArrayList<Designer> listOfDesigners = new ArrayList<Designer>();
         ArrayList<Puzzle> listOfPuzzles = new ArrayList<Puzzle>();
         ArrayList<SoftToy> listOfSoftToys = new ArrayList<SoftToy>();
-        generateDesigners(listOfDesigners);
-        generatePuzzles(listOfPuzzles);
-        generateSoftToys(listOfSoftToys);
+        File file1=null;
+        File file2=null;
+        File file3=null;
+        try {
+            file1= new File("designers.txt");
+            generateDesigners(listOfDesigners,file1);
+            if(!file1.exists()){
+                file1.createNewFile();
+            }
+
+        }catch (IOException e){
+            System.out.println("Помилка! "+e);
+        }
+        try {
+            file2= new File("puzzles.txt");
+            generatePuzzles(listOfPuzzles,file2);
+            if(!file2.exists()){
+                file2.createNewFile();
+            }
+
+        }catch (IOException e){
+            System.out.println("Помилка! "+e);
+        }
+        try {
+            file3= new File("softtoys.txt");
+            generateSoftToys(listOfSoftToys,file3);
+            if(!file3.exists()){
+                file3.createNewFile();
+            }
+
+        }catch (IOException e){
+            System.out.println("Помилка! "+e);
+        }
+
         Check check= new Check((long) (Math.random()*1000000000),0);
         while (true){
 
@@ -31,28 +63,28 @@ public class PayDesk {
             int chooseItem=in.nextInt();
             switch (chooseItem){
                 case 1:
-                    buyDesigner(listOfDesigners,check);
+                    buyDesigner(listOfDesigners,check,file1);
                     break;
                 case 2:
-                    buyPuzzle(listOfPuzzles,check);
+                    buyPuzzle(listOfPuzzles,check,file2);
                     break;
                 case 3:
-                    buySoftToy(listOfSoftToys,check);
+                    buySoftToy(listOfSoftToys,check,file3);
                     break;
                 case 4:
-                    addToy(listOfDesigners,listOfPuzzles,listOfSoftToys);
+                    addToy(listOfDesigners,listOfPuzzles,listOfSoftToys,file1,file2,file3);
                     break;
                 case  5:
                     printCheck(check);
                     System.out.println("Вихід");
                     System.exit(0);
+
                 default:
                     System.out.println("Помилка вибору, спробуйте ще раз!");
             }
         }
     }
-
-    private static void generateDesigners(ArrayList<Designer> list){
+    private static void generateDesigners(ArrayList<Designer> list,File file) throws FileNotFoundException {
         Calendar calendarFirst=Calendar.getInstance();
         Calendar calendarSecond=Calendar.getInstance();
         Date date=new Date();
@@ -62,8 +94,13 @@ public class PayDesk {
         calendarSecond.add(Calendar.DATE,-8);
         list.add(new Designer("1a", 120.5F,calendarFirst.getTime(),100));
         list.add(new Designer("1b", 150.5F,calendarSecond.getTime(),120));
+        PrintWriter pw=new PrintWriter(file);
+        for(Designer disigner:list){
+            pw.println(disigner);
+        }
+        pw.close();
     }
-    private static void generatePuzzles(ArrayList<Puzzle> list){
+    private static void generatePuzzles(ArrayList<Puzzle> list,File file) throws FileNotFoundException {
         Calendar calendarFirst=Calendar.getInstance();
         Calendar calendarSecond=Calendar.getInstance();
         Date date=new Date();
@@ -73,8 +110,13 @@ public class PayDesk {
         calendarSecond.add(Calendar.DATE,-5);
         list.add(new Puzzle("2a",80.3F,calendarFirst.getTime(),200));
         list.add(new Puzzle("2b",120.3F,calendarSecond.getTime(),500));
+        PrintWriter pw=new PrintWriter(file);
+        for(Puzzle puzzle:list){
+            pw.println(puzzle);
+        }
+        pw.close();
     }
-    private static void generateSoftToys(ArrayList<SoftToy> list){
+    private static void generateSoftToys(ArrayList<SoftToy> list,File file) throws FileNotFoundException{
         Calendar calendarFirst=Calendar.getInstance();
         Calendar calendarSecond=Calendar.getInstance();
         Date date=new Date();
@@ -84,14 +126,21 @@ public class PayDesk {
         calendarSecond.add(Calendar.DATE,-7);
         list.add(new SoftToy("3a",250.4F,calendarFirst.getTime(),true));
         list.add(new SoftToy("3b",120.6F,calendarSecond.getTime(),false));
+        PrintWriter pw=new PrintWriter(file);
+        for(SoftToy softToy:list){
+            pw.println(softToy);
+        }
+        pw.close();
     }
-    private static void buyDesigner(ArrayList<Designer> list, Check check) throws ArrayIndexOutOfBoundsException {
+    private static void buyDesigner(ArrayList<Designer> list, Check check,File file)
+            throws ArrayIndexOutOfBoundsException, IOException {
         Scanner in= new Scanner(System.in);
         System.out.println("Оберіть конструктор:");
         int i=1;
-        Iterator<Designer> designerIterator =list.iterator();
-        while(designerIterator.hasNext()){
-            System.out.println(i+". "+designerIterator.next());
+        BufferedReader br=new BufferedReader(new FileReader("designers.txt"));
+        String line;
+        while((line=br.readLine())!=null){
+            System.out.println(i+". "+line);
             i+=1;
         }
         int choose=in.nextInt();
@@ -101,15 +150,22 @@ public class PayDesk {
         check.setSum(list.get(choose-1).getPrice());
         check.setPurchases(list.get(choose-1));
         list.remove(choose-1);
+        PrintWriter pw=new PrintWriter(file);
+        for(Designer disigner:list){
+            pw.println(disigner);
+        }
+        pw.close();
         System.out.println("Дякуємо за покупку!");
     }
-    private static void buyPuzzle(ArrayList<Puzzle> list,Check check) throws ArrayIndexOutOfBoundsException{
+    private static void buyPuzzle(ArrayList<Puzzle> list,Check check,File file)
+            throws ArrayIndexOutOfBoundsException, IOException {
         Scanner in= new Scanner(System.in);
         System.out.println("Оберіть пазли:");
         int i=1;
-        Iterator<Puzzle> puzzleIterator =list.iterator();
-        while(puzzleIterator.hasNext()){
-            System.out.println(i+". "+puzzleIterator.next());
+        BufferedReader br=new BufferedReader(new FileReader("puzzles.txt"));
+        String line;
+        while((line=br.readLine())!=null){
+            System.out.println(i+". "+line);
             i+=1;
         }
         int choose=in.nextInt();
@@ -119,15 +175,22 @@ public class PayDesk {
         check.setSum(list.get(choose-1).getPrice());
         check.setPurchases(list.get(choose-1));
         list.remove(choose-1);
+        PrintWriter pw=new PrintWriter(file);
+        for(Puzzle puzzle:list){
+            pw.println(puzzle);
+        }
+        pw.close();
         System.out.println("Дякуємо за покупку!");
     }
-    private static void buySoftToy(ArrayList<SoftToy> list,Check check) throws ArrayIndexOutOfBoundsException{
+    private static void buySoftToy(ArrayList<SoftToy> list,Check check,File file)
+            throws ArrayIndexOutOfBoundsException, IOException {
         Scanner in= new Scanner(System.in);
         System.out.println("Оберіть м'яку іграшку:");
         int i=1;
-        Iterator<SoftToy> softToyIterator =list.iterator();
-        while(softToyIterator.hasNext()){
-            System.out.println(i+". "+softToyIterator.next());
+        BufferedReader br=new BufferedReader(new FileReader("softtoys.txt"));
+        String line;
+        while((line=br.readLine())!=null){
+            System.out.println(i+". "+line);
             i+=1;
         }
         int choose=in.nextInt();
@@ -137,11 +200,16 @@ public class PayDesk {
         check.setSum(list.get(choose-1).getPrice());
         check.setPurchases(list.get(choose-1));
         list.remove(choose-1);
+        PrintWriter pw=new PrintWriter(file);
+        for(SoftToy softToy:list){
+            pw.println(softToy);
+        }
+        pw.close();
         System.out.println("Дякуємо за покупку!");
     }
     private static void addToy(ArrayList<Designer> list1,
                                ArrayList<Puzzle> list2,
-                               ArrayList<SoftToy> list3) {
+                               ArrayList<SoftToy> list3,File file1,File file2,File file3) throws FileNotFoundException {
         System.out.println("1. Додати конструктор");
         System.out.println("2. Дадати пазли");
         System.out.println("3. Додати м'яку іграшку");
@@ -149,20 +217,21 @@ public class PayDesk {
         int chooseItem = in.nextInt();
         switch (chooseItem) {
             case 1:
-                addDesigner(list1);
+                addDesigner(list1,file1);
                 break;
             case 2:
-                addPuzzle(list2);
+                addPuzzle(list2,file2);
                 break;
             case 3:
-                addSoftToy(list3);
+                addSoftToy(list3,file3);
                 break;
             default:
                 System.out.println("Помилка вибору, спробуйте ще раз!");
                 break;
         }
     }
-    private static void addDesigner(ArrayList<Designer> list) throws InputMismatchException{
+    private static void addDesigner(ArrayList<Designer> list,File file) throws InputMismatchException,
+            FileNotFoundException{
         Date date=new Date();
         Scanner in= new Scanner(System.in);
         System.out.println("Введіть артикул:");
@@ -173,13 +242,18 @@ public class PayDesk {
             System.out.println("Введіть кількість елементів конструктора:");
             int elements= in.nextInt();
             list.add(new Designer(article,price,date,elements));
+            PrintWriter pw = new PrintWriter(file);
+            for(Designer designer:list){
+                pw.println(designer);
+            }
+            pw.close();
         }
         catch (InputMismatchException e){
             System.err.println("Неправильний тип даних");
             e.printStackTrace();
         }
     }
-    private static void addPuzzle(ArrayList<Puzzle> list) throws InputMismatchException{
+    private static void addPuzzle(ArrayList<Puzzle> list,File file) throws InputMismatchException{
         Date date=new Date();
         Scanner in= new Scanner(System.in);
         System.out.println("Введіть артикул:");
@@ -190,13 +264,18 @@ public class PayDesk {
             System.out.println("Введіть кількість пазлів:");
             int elements= in.nextInt();
             list.add(new Puzzle(article,price,date,elements));
+            PrintWriter pw = new PrintWriter(file);
+            for(Puzzle puzzle:list){
+                pw.println(puzzle);
+            }
+            pw.close();
         }
-        catch (InputMismatchException e){
+        catch (InputMismatchException | FileNotFoundException e){
             System.err.println("Неправельний тип даних");
             e.printStackTrace();
         }
     }
-    private static void addSoftToy(ArrayList<SoftToy> list){
+    private static void addSoftToy(ArrayList<SoftToy> list,File file){
         Date date=new Date();
         Scanner in= new Scanner(System.in);
         System.out.println("Введіть артикул:");
@@ -207,8 +286,13 @@ public class PayDesk {
             System.out.println("Введіть чи іграшка має здатність говорити:");
             Boolean speak= in.nextBoolean();
             list.add(new SoftToy(article,price,date,speak));
+            PrintWriter pw = new PrintWriter(file);
+            for(SoftToy softToy:list){
+                pw.println(softToy);
+            }
+            pw.close();
         }
-        catch (InputMismatchException e){
+        catch (InputMismatchException | FileNotFoundException e){
             System.err.println("Неправельний тип даних");
             e.printStackTrace();
         }
